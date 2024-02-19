602250fae47e353b7022b1c8365c3505bda23998
Tobias Gleiter <gleiter.tobias@outlook.de>
2024-02-19 20:27:14 +0100

    feat: redis go example

---
+++ examples/redis-go/README.md
@@ -0,0 +1,40 @@
+# Use Redis with Go
+
+## Checklist to setup redis yourself
+
+1. Setup your go project `go mod init github.com/my/repo`
+2. Run `go get github.com/redis/go-redis/v9` to install go for redis
+3. Connect to Redis using this code:
+
+```Go
+import (
+	"context"
+	"fmt"
+	"github.com/redis/go-redis/v9"
+)
+
+func main() {
+    client := redis.NewClient(&redis.Options{
+        Addr:	  "localhost:6379",
+        Password: "", // no password set
+        DB:		  0,  // use default DB
+    })
+}
+```
+
+4. Store and retrieve simple string
+
+```Go
+ctx := context.Background()
+
+err := client.Set(ctx, "foo", "bar", 0).Err()
+if err != nil {
+    panic(err)
+}
+
+val, err := client.Get(ctx, "foo").Result()
+if err != nil {
+    panic(err)
+}
+fmt.Println("foo", val)
+```
--- examples/redis-go/main.go
+++ examples/redis-go/main.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"github.com/redis/go-redis/v9" // 2. Import the Redis package
+)
 
 func main() {
-    fmt.Println("Hello, World!")
+    // 3. Connect to the Redis client
+    client := redis.NewClient(&redis.Options{
+        Addr:     "localhost:6379",
+        Password: "",
+        DB:       0,
+    })
+
+    // Create a context
+    ctx := context.Background()
+
+    // Set a key-value pair in Redis
+    err := client.Set(ctx, "foo", "bar", 0).Err()
+    if err != nil {
+        panic(err) // If there's an error, panic
+    }
+
+    // Get the value of the "foo" key from Redis
+    val, err := client.Get(ctx, "foo").Result()
+    if err != nil {
+        panic(err) // If there's an error, panic
+    }
+    
+    // Print the value retrieved from Redis
+    fmt.Println("foo", val)
 }
