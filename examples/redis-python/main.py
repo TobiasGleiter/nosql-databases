21a8a471363cbd55339bd3ec8f8f07c18939384c
Tobias Gleiter <gleiter.tobias@outlook.de>
2024-02-25 11:34:47 +0100

    feat: examples

--- examples/redis-python/main.py
+++ examples/redis-python/main.py
@@ -1,10 +1,10 @@
 import redis
-
 # Connect to localhost on port 6379
 # To receive decoded strings, set decode_responses=True
 r = redis.Redis(host='localhost', port=6379, decode_responses=True)
 r.set('foo', 'bar')
-# True
-
-# bar
-print(r.get('foo'))
+r.get('foo')
+r.setnx('foo', 'bar')
+r.getset('foo', 'rab')
+r.mset({'fo': 'ba', 'f': 'b'})
+r.mget({'foo', 'fo', 'f'})
